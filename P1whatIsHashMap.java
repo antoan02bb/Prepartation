c2d85645775fb54b04933ba13086379b2bd9a41c
antoan02bb <100437385+antoan02bb@users.noreply.github.com>
2022-03-31 12:55:20 +0300

    Add files via upload

---
+++ P1whatIsHashMap.java
@@ -0,0 +1,16 @@
+package AssociativeArrays;
+//Arrays store items as an order collection. We access them with an index number
+// A HashMap stores items in "key/value" pairs and can be accessed by an index of another type
+/*
+ One object is used as a key (index) to another object (value). It can store different types:
+ String keys and Integer values, or the same types, like: String keys and String keys and String values
+ */
+
+import java.util.HashMap;
+
+public class P1whatIsHashMap<S, S1> {
+
+     // Create a HashMap object called capitalCities that wil store String keys and String values
+     HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+}
